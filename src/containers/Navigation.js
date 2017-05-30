3fba9df541156c746e705dcbfdd75375e6c4124b
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2017-05-30 22:08:32 +0300

    Created user dropdown-menu to navbar. Fixed proper way to import bootswatch.

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {LinkContainer} from 'react-router-bootstrap';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, Glyphicon} from 'react-bootstrap';
 import {logout as LogoutAction} from '../actions/SessionActions';
 
 class Navigation extends Component{
@@ -34,13 +34,22 @@ class Navigation extends Component{
             <LinkContainer to="/settings"><NavItem eventKey={ 5 }>Settings</NavItem></LinkContainer>
           </Nav>
           <Nav pullRight>
-            <NavItem onSelect={ this.handleLogout }>Logout</NavItem>
+            <NavDropdown eventKey={ 6 } title={<span><Glyphicon glyph='user' /> {this.props.Session.username}</span>} id="userDropdown">
+              <LinkContainer to="/profile"><NavItem eventKey={ 6.1 }>Profile</NavItem></LinkContainer>
+              <NavItem eventKey={ 6.2 } onSelect={ this.handleLogout }>Logout</NavItem>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    Session: state.Session
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -51,7 +60,8 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 Navigation.propTypes = {
+  Session: PropTypes.object.isRequired,
   LogoutAction: PropTypes.func.isRequired
 };
 
-export default connect(null, mapDispatchToProps)(Navigation);
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './containers/App';
 import store from './store';
+import 'bootswatch/paper/bootstrap.min.css';
+import './styles.css';
 
 render(
   <Provider store={ store }>
